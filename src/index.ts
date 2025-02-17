84defd8b513057d69eaf2dbe115cbf9cb0332f00
Kulik → Stas <stskulik@gmail.com>
2025-02-17 23:11:13 +0300

    Add apollo server

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  type Post {
+    title: String
+  }
+
+  type Query {
+    posts: [Post]
+  }
+`;
+
+const posts = [
+  {
+    title: 'The Awakening',
+  },
+  {
+    title: 'City of Glass',
+  },
+];
+
+const resolvers = {
+  Query: {
+    posts: () => posts,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
